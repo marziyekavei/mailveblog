063d9421b337fa116a61732b39cffc4f06fde08c
marziyekavei <138191022+marziyekavei@users.noreply.github.com>
2025-01-21 21:48:31 +0330

    server mjstc

--- src/api/apiSlice.jsx
+++ src/api/apiSlice.jsx
@@ -1,18 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const SUPABASE_URL = "https://mcggfakkqawjnyaqqnju.supabase.co";
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jZ2dmYWtrcWF3am55YXFxbmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNzg4NTAsImV4cCI6MjA1Mjk1NDg1MH0.XOHfjWTcBlxxMM2TjgyWqnTEjH8m1OlWRTInr8KY_Xo";
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl: `${SUPABASE_URL}/rest/v1`,
-        prepareHeaders: (headers) => {
-            // افزودن هدر برای دسترسی به Supabase
-            headers.set('Authorization', `Bearer ${API_KEY}`);
-            headers.set('apikey', API_KEY);
-            return headers;
-        }
-    }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://my-json-server.typicode.com/marziyekavei/veblog" }),
     tagTypes: ["BLOG", "USER"],
     endpoints: builder => ({
         getBlogs: builder.query({
@@ -23,7 +13,7 @@ export const apiSlice = createApi({
             ]
         }),
         getBlog: builder.query({
-            query: (initialBlogId) => `/blogs?id=eq.${initialBlogId}`,
+            query: (initialBlogId) => `/blogs/${initialBlogId}`,
             providesTags: (result, error, arg) =>
                 [{ type: "BLOG", id: arg }],
         }),
@@ -37,7 +27,7 @@ export const apiSlice = createApi({
         }),
         editBlog: builder.mutation({
             query: blog => ({
-                url: `/blogs?id=eq.${blog.id}`,
+                url: `/blogs/${blog.id}`,
                 method: "PUT",
                 body: blog
             }),
@@ -46,7 +36,7 @@ export const apiSlice = createApi({
         }),
         deleteBlog: builder.mutation({
             query: blogId => ({
-                url: `/blogs?id=eq.${blogId}`,
+                url: `/blogs/${blogId}`,
                 method: "DELETE"
             }),
             invalidatesTags: ["BLOG"],
@@ -61,4 +51,5 @@ export const {
     useEditBlogMutation,
     useDeleteBlogMutation } = apiSlice;
 
-//https://my-json-server.typicode.com/marziyekavei/veblog
+    //
+    
--- src/services/blogsServices.js
+++ src/services/blogsServices.js
@@ -1,58 +1,49 @@
 import axios from "axios";
 
-const SUPABASE_URL = "https://mcggfakkqawjnyaqqnju.supabase.co";
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jZ2dmYWtrcWF3am55YXFxbmp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczNzg4NTAsImV4cCI6MjA1Mjk1NDg1MH0.XOHfjWTcBlxxMM2TjgyWqnTEjH8m1OlWRTInr8KY_Xo";
-
-const headers = {
-    "Content-Type": "application/json",
-    "Authorization": `Bearer ${API_KEY}`,  // اضافه کردن هدر احراز هویت
-    "apikey": API_KEY,  // اضافه کردن کلید API به عنوان هدر
-};
+const SERVER_URL = "https://my-json-server.typicode.com/marziyekavei/veblog";
 
 export const getAllBlogs = () => {
-    const url = `${SUPABASE_URL}/blogs`;
-    return axios.get(url, {headers});
+    const url = `${SERVER_URL}/blogs`;
+    return axios.get(url);
 };
 
 export const getBlog = (blogId) => {
-    const url = `${SUPABASE_URL}/blogs?id=eq.${blogId}`;
-    return axios.get(url, {headers});
+    const url = `${SERVER_URL}/blogs/${blogId}`;
+    return axios.get(url);
 };
 
 
 export const getAllUsers = () => {
-    const url = `${SUPABASE_URL}/users`;
-    return axios.get(url, {headers})
+    const url = `${SERVER_URL}/users`;
+    return axios.get(url)
 };
 
 export const getUser = (userId) => {
-    const url = `${SUPABASE_URL}/users?id=eq.${userId}`;
-    return axios.get(url, {headers});
+    const url = `${SERVER_URL}/users/${userId}`;
+    return axios.get(url);
 };
 
 export const deleteUser = (userId) => {
-    const url = `${SUPABASE_URL}/users?id=eq.${userId}`;
-    return axios.delete(url, {headers});
+    const url = `${SERVER_URL}/users/${userId}`;
+    return axios.delete(url);
 };
 
 export const createUser = (user) => {
     const url = `${SERVER_URL}/users`;
-    return axios.post(url, user, {headers});
+    return axios.post(url, user);
 };
 
 export const createBlog = (blog) => {
     const url = `${SERVER_URL}/blogs`;
-    return axios.post(url, blog, {headers});
+    return axios.post(url, blog);
 };
 
 export const updateBlog = (blog, blogId) => {
-    const url = `${SUPABASE_URL}/blogs?id=eq.${blogId}`;
-    return axios.put(url, blog, {headers});
+    const url = `${SERVER_URL}/blogs/${blogId}`;
+    return axios.put(url, blog);
 };
 
 export const deleteBlog = (blogId) => {
-    const url = `${SUPABASE_URL}/blogs?id=eq.${blogId}`;
-    return axios.delete(url, {headers});
+    const url = `${SERVER_URL}/blogs/${blogId}`;
+    return axios.delete(url);
 };
-
-//https://api.jsonbin.io/v3/b/678fb134ad19ca34f8f20c9e
