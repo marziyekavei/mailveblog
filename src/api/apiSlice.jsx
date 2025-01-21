29393b1915c1b4deca4b1758a2bc48ae913b3070
marziyekavei <138191022+marziyekavei@users.noreply.github.com>
2025-01-21 15:32:24 +0330

    initial

--- src/api/apiSlice.jsx
+++ src/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://my-json-server.typicode.com/marziyekavei/veblog" }),
     tagTypes: ["BLOG", "USER"],
     endpoints: builder => ({
         getBlogs: builder.query({
