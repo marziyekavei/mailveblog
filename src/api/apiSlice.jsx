74e882ef37c5bbdc2130462aaae8d268aa1d88d5
marziyekavei <138191022+marziyekavei@users.noreply.github.com>
2025-01-24 19:22:04 +0330

    server giltch

--- src/api/apiSlice.jsx
+++ src/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: "https://my-json-server.typicode.com/marziyekavei/veblog" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://zircon-airy-celestite.glitch.me" }),
     tagTypes: ["BLOG", "USER"],
     endpoints: builder => ({
         getBlogs: builder.query({
--- src/services/blogsServices.js
+++ src/services/blogsServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = "https://my-json-server.typicode.com/marziyekavei/veblog";
+const SERVER_URL = "https://zircon-airy-celestite.glitch.me";
 
 export const getAllBlogs = () => {
     const url = `${SERVER_URL}/blogs`;
@@ -47,3 +47,7 @@ export const deleteBlog = (blogId) => {
     const url = `${SERVER_URL}/blogs/${blogId}`;
     return axios.delete(url);
 };
+
+
+//https://zircon-airy-celestite.glitch.me
+//https://my-json-server.typicode.com/marziyekavei/veblog
